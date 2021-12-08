2299c00de34d7a17040b31fc48372f435b0ca8a1
Haemin Jeong <jhm0828@gmail.com>
2021-12-08 20:40:12 +0900

    HelloController 롬복 적용하기

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,19 @@ public class HelloControllerTest {
                 // mvc.perform의 결과를 검증.
                 .andExpect(MockMvcResultMatchers.status().isOk())  //응답 코드가 200 OK인지 검증
                 .andExpect(MockMvcResultMatchers.content().string(hello)); //mvc.perform의 결과를 검증. 응답 본문의 내용이 hello인지 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                        .param("name", name) //API를 테스트할 때 요청 파라미터를 설정(문자열 타입만 가능)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //jsonPath는 JSON 응답 값을 필드 별로 검증할 수 있는 메서드. $를 기준으로 필드명을 명시한다.
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(name)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", Matchers.is(amount)));
     }
 }
