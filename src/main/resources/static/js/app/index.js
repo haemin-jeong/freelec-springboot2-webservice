efb1ad998c914c0aa218551f9a71ad8ff235b9d2
Haemin Jeong <jhm0828@gmail.com>
2021-12-14 22:04:03 +0900

    게시글 수정, 삭제 화면 만들기

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,15 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         })
+
+        //btn-update 아이디를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트 등록
+        $('#btn-update').on('click', function () {
+            _this.update();
+        })
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        })
     },
     save : function () {
         var data = {
@@ -23,6 +32,42 @@ var main = {
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error))
+        });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function() {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
         });
     }
 };
