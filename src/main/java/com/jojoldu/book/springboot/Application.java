e0de50aa34e777bdd9aba0b0d8ce90fc4e98a8eb
Haemin Jeong <jhm0828@gmail.com>
2021-12-21 16:30:45 +0900

    기존 테스트에 시큐리티 적용하기

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,9 +2,10 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing 가 삭제됨 -> @EnableJpaAuditing 을 사용하려면 최소 1개의 @Entity 클래스가 있어야한다.
+//그런데 테스트에서 @WebMvcTest 를 사용하면 @Entity 를 스캔할 수 없기 때문에 'java.lang.IllegalArgumentException: JPA metamodel must not be empty!' 에러가 발생한다.
+//그래서 @EnableJpaAuditing 을 JpaConfig 클래스로 별도 분리하였다.
 @SpringBootApplication //스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성 모두 자동으로 설정된다.
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,23 +1,34 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HelloController.class)
+/*
+WebMvcConfigurerAdapter, WebMvcConfigurer 를 비롯한 @ControllerAdvice, @Controller 를 읽는다.
+즉, @Repository, @Service, @Component 는 스캔 대상이 아니다.
+ */
+@WebMvcTest(controllers = HelloController.class,
+    //@WebMvcTest 는 @Service 를 스캔하지 못하기 때문에 SecurityConfig는 읽을 수 있지만, SecurityConfig 를 생성하기 위해 필요한 CustomOAuth2UserService 를 읽을 수가 없어 에러가 발생하기 때문에 SecurityConfig 를 스캔 대상에서 제외한다.
+    excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
     //웹 API를 테스트할 때 사용, 스프링 MVC 테스트의 시작점
     //HTTP GET, POST 등에 대한 API 테스트를 할 수 있다.
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws  Exception {
         String hello = "hello";
@@ -28,6 +39,7 @@ public class HelloControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string(hello)); //mvc.perform의 결과를 검증. 응답 본문의 내용이 hello인지 검증
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
